2db3f3d68602993622c03b3d86d65ab586d7305e
dimasrisky <rizkyalphazee@gmail.com>
2024-11-29 09:34:13 +0700

    change accent color

--- src/components/BubbleChat.tsx
+++ src/components/BubbleChat.tsx
@@ -10,7 +10,7 @@ interface BubbleChatProps {
 const BubbleChat: React.FC<BubbleChatProps> = ({ role, text }) => {
   return (
     <div className="w-full flex " style={{ justifyContent: role === 'user' ? 'flex-end' : 'flex-start' }}>
-        <div className={`${role === 'user' ? 'px-[22px]' : 'px-1 w-[90%]' } py-[12px] rounded-full`} style={{ backgroundColor: role === 'user' ? '#282A2C' : 'transparent' }}>
+        <div className={`${role === 'user' ? 'px-[22px]' : 'px-1 w-[90%]' } py-[12px] rounded-full`} style={{ backgroundColor: role === 'user' ? '#161820' : 'transparent' }}>
             <div className="text-primary-white text-[13px] leading-6 md:text-[15px] md:leading-9">
               <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>{text}</Markdown>
             </div>
