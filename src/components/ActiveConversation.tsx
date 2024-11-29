18ad19cd2a634a88f8d2cfe415bd2bec9d8a0df7
dimasrisky <rizkyalphazee@gmail.com>
2024-11-29 09:51:03 +0700

    add initial chat

--- src/components/ActiveConversation.tsx
+++ src/components/ActiveConversation.tsx
@@ -12,7 +12,9 @@ const ActiveConversation: React.FC<ActiveConversationProps> = ({ chats }) => {
   const { isLoading } = useContext(ChatContext)
   return (
     <div className="w-full h-[90%] flex flex-col gap-3 overflow-y-scroll py-4 md:gap-8" style={{ scrollbarWidth: 'none'}}>
-      {chats?.map((chat, index) => <BubbleChat key={index} role={chat.role} text={chat.content} />)}
+      {chats?.map((chat, index) => {
+        if(index != 0) return <BubbleChat key={index} role={chat.role} text={chat.content} />
+      })}
       {isLoading && <BubbleChatLoading />}
    </div>
   )
