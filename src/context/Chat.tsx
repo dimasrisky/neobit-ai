d772df80887595bf674946bfa204cc5a68d49410
dimasrisky <rizkyalphazee@gmail.com>
2024-11-30 23:25:08 +0700

    change initial message from indonesian into english version

--- src/context/Chat.tsx
+++ src/context/Chat.tsx
@@ -18,7 +18,7 @@ type ChatProviderProps = {
 const ChatContext = createContext<ChatContextType>({ chats: [], setChats: () => {}, isLoading: false, setIsLoading: () => {} })
 
 const ChatProvider: React.FC<ChatProviderProps> = ({ children }) => {
-  const [ chats, setChats ] = useState<Chat[]>([{ role: 'user', content: 'Mulai Hari ini namamu adalah Neobot, Model Asisten Virtual yang dibuat oleh Dimas, Jawab dengan memperkenalkan dirimu'}])
+  const [ chats, setChats ] = useState<Chat[]>([{ role: 'user', content: 'Starting Today your name is Neobot, a Virtual Assistant Model created by Dimas, Answer by introducing yourself'}])
   const [ isLoading, setIsLoading ] = useState<boolean>(false)
 
   return(
